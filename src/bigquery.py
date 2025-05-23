9ecee6e680027f290c07761902734f5d2502e1fb
PeDiot <p.emmanuel.diot@gmail.com>
2025-05-23 11:23:44 +0200

    Add insert_unique function to handle unique row insertion in BigQuery

--- src/bigquery.py
+++ src/bigquery.py
@@ -1,7 +1,10 @@
-from typing import Dict
+from typing import Dict, List, Tuple
+import time
 
 from google.cloud import bigquery
 from google.oauth2 import service_account
+
+from .queries import make_bigquery_merge_query
 
 
 def init_client(credentials_dict: Dict) -> bigquery.Client:
@@ -18,7 +21,12 @@ def init_client(credentials_dict: Dict) -> bigquery.Client:
     )
 
 
-def insert(client: bigquery.Client, dataset_id: str, table_id: str, rows: Dict) -> bool:
+def insert(
+    client: bigquery.Client,
+    dataset_id: str,
+    table_id: str,
+    rows: List[Dict],
+) -> bool:
     try:
         errors = client.insert_rows_json(
             table=f"{dataset_id}.{table_id}", json_rows=rows
@@ -28,3 +36,57 @@ def insert(client: bigquery.Client, dataset_id: str, table_id: str, rows: Dict) 
 
     except:
         return False
+
+
+def insert_unique(
+    client: bigquery.Client,
+    dataset_id: str,
+    table_id: str,
+    rows: List[Dict],
+    unique_field: str,
+) -> Tuple[int, bool]:
+    try:
+        if not rows:
+            return 0
+
+        project_id = client.project
+        temp_table_id = f"temp_{table_id}_{int(time.time())}"
+        temp_table_ref = f"{project_id}.{dataset_id}.{temp_table_id}"
+        target_table_ref = f"{project_id}.{dataset_id}.{table_id}"
+
+        target_table = client.get_table(target_table_ref)
+        schema = target_table.schema
+
+        temp_table = bigquery.Table(temp_table_ref, schema=schema)
+        client.create_table(temp_table, exists_ok=True)
+
+        errors = client.insert_rows_json(temp_table_ref, rows)
+        if errors:
+            client.delete_table(temp_table_ref)
+
+            return 0, False
+
+        query = make_bigquery_merge_query(
+            target_table_ref=target_table_ref,
+            temp_table_ref=temp_table_ref,
+            unique_field=unique_field,
+            rows=rows,
+        )
+
+        query_job = client.query(query)
+        result = query_job.result()
+        num_inserted = result.num_dml_affected_rows
+
+        client.delete_table(temp_table_ref)
+
+        return num_inserted, True
+
+    except Exception as e:
+        print(f"Error in insert_unique_rows: {str(e)}")
+
+        try:
+            client.delete_table(temp_table_ref)
+        except:
+            pass
+
+        return 0, False
